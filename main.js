0e68058a78f39ec56fd8b553c43358240c5c9bcc
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-05 20:29:00 +0200

    Enable dev tools for now
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
