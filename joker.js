600cc7567b7e8c5e1a4dfac8d381a3a3b498466c
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-06 20:42:16 +0200

    Use pyjokes[0]
    Thanks to nyasaki_de for suggesting programmers jokes.
    
    [0]: https://pyjok.es/#install
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- joker.js
+++ joker.js
@@ -1,38 +1,19 @@
-var giveMeAJoke = require('give-me-a-joke')
+'use strict'
 
-function getRandomInt (min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  let date = new Date()
-  let seed = date.getMonth() + date.getFullYear() + date.getMinutes() + date.getMilliseconds() + date.getSeconds()
-  return Math.floor(Math.random(seed) * (max - min)) + min
-}
+const { exec } = require('child_process')
 
 function randomJoke (callback) {
-  let pick = getRandomInt(1, 3)
+  exec('/usr/local/bin/pyjoke', (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return
+    }
 
-  switch (pick) {
-    case 1:
-      console.log('random dad joke')
-      giveMeAJoke.getRandomDadJoke(function (joke) {
-        callback(joke)
-      })
-      break
-    case 2:
-      console.log('random Chuck Norris joke')
-      giveMeAJoke.getRandomCNJoke(function (joke) {
-        callback(joke)
-      })
-      break
-    case 3:
-      console.log('random Jackie Chan joke')
-      var fn = 'Jackie'
-      var ln = 'Chan'
-      giveMeAJoke.getCustomJoke(fn, ln, function (joke) {
-        callback(joke)
-      })
-      break
-  }
+    callback(stdout)
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+  })
 }
 
 module.exports = {
