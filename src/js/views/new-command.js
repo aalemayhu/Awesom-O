cf6fa44eb9e4e8b5e1c2f7d27a58dc6384a65417
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-07 20:14:59 +0200

    Use jquery in new-command.js

--- src/js/views/new-command.js
+++ src/js/views/new-command.js
@@ -3,7 +3,34 @@
 const { remote, ipcRenderer } = require('electron')
 var $ = require('jQuery')
 
-function newCommandSubmit () {
+let selectedCommand = remote.getGlobal('selectedCommand')
+if (selectedCommand) {
+  let cmd = remote.getGlobal('commands').find(function (cmd) {
+    if (cmd.name === selectedCommand) {
+      return cmd
+    }
+  })
+
+  if (cmd) {
+    console.log('prefix:')
+    console.log(cmd)
+    document.querySelector('#command-name').value = cmd.name
+    document.querySelector('#command-description').value = cmd.description
+    document.querySelector('#command-enabled').checked = cmd.enabled
+
+    if (cmd.type === 'builtin') {
+      document.querySelector('#form-group-command-value').remove()
+      document.querySelector('#form-group-command-type').remove()
+      document.querySelector('#command-name').readOnly = true
+      document.querySelector('#command-description').readOnly = true
+    } else {
+      document.querySelector('.custom-select').selectedIndex = cmd.type === 'string' ? 0 : 1
+      document.querySelector('#command-value').value = cmd.value
+    }
+  }
+}
+
+$('#new-command-submit').click(function () {
   var cmd = {}
   let typeSelector = document.querySelector('.custom-select')
   if (typeSelector) {
@@ -20,46 +47,12 @@ function newCommandSubmit () {
   cmd.description = document.querySelector('#command-description').value
   cmd.enabled = document.querySelector('#command-enabled').checked
   ipcRenderer.send('save-command', cmd)
-}
+})
 
-function newCommandCancel () {
+$('#new-command-cancel').click(function () {
   console.log('newCommandCancel()')
   remote.getCurrentWindow().loadFile('src/pages/index.html')
-}
-
-if (document.querySelector('#new-command-submit')) {
-  document.querySelector('#new-command-submit').addEventListener('click', newCommandSubmit)
-
-  let selectedCommand = remote.getGlobal('selectedCommand')
-  if (selectedCommand) {
-    let cmd = remote.getGlobal('commands').find(function (cmd) {
-      if (cmd.name === selectedCommand) {
-        return cmd
-      }
-    })
-
-    if (cmd) {
-      console.log('prefix:')
-      console.log(cmd)
-      document.querySelector('#command-name').value = cmd.name
-      document.querySelector('#command-description').value = cmd.description
-      document.querySelector('#command-enabled').checked = cmd.enabled
-
-      if (cmd.type === 'builtin') {
-        document.querySelector('#form-group-command-value').remove()
-        document.querySelector('#form-group-command-type').remove()
-        document.querySelector('#command-name').readOnly = true
-        document.querySelector('#command-description').readOnly = true
-      } else {
-        document.querySelector('.custom-select').selectedIndex = cmd.type === 'string' ? 0 : 1
-        document.querySelector('#command-value').value = cmd.value
-      }
-    }
-  }
-}
-if (document.querySelector('#new-command-cancel')) {
-  document.querySelector('#new-command-cancel').addEventListener('click', newCommandCancel)
-}
+})
 
 $('#new-command-delete').click(function () {
   ipcRenderer.send('delete-command', $('#channel-name').val())
