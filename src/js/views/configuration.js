4b00c728bea8ce3ebd71ef5630d36f5d91162a3d
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-07 20:58:58 +0200

    Add primitive input validation to the configuration page

--- src/js/views/configuration.js
+++ src/js/views/configuration.js
@@ -3,26 +3,6 @@
 const { remote, ipcRenderer } = require('electron')
 var $ = require('jQuery')
 
-function configurationSubmit () {
-  let c = {}
-  c.name = document.querySelector('#channel-name').value.toLowerCase()
-  c.bot = document.querySelector('#channel-bot').value.toLowerCase()
-  c.oauth = document.querySelector('#channel-oauth').value
-  c.autoconnect = document.querySelector('#channel-autoconnect').checked
-  c.silent = document.querySelector('#channel-notifications').checked
-  c.standupReminder = document.querySelector('#channel-notification-break').checked
-
-  if (!c.name.startsWith('#')) {
-    c.name = `'#${c.name}`
-  }
-  ipcRenderer.send('save-configuration', c)
-}
-
-function configurationCancel () {
-  console.log('configurationCancel()')
-  remote.getCurrentWindow().loadFile('src/pages/index.html')
-}
-
 function render () {
   let c = remote.getGlobal('config')
   document.querySelector('#channel-name').value = c.name
@@ -32,15 +12,39 @@ function render () {
   document.querySelector('#channel-notifications').checked = c.silent
   document.querySelector('#channel-notification-break').checked = c.standupReminder
 }
+render()
 
-if (document.querySelector('#new-configuration-submit')) {
-  document.querySelector('#new-configuration-submit').addEventListener('click', configurationSubmit)
-  render()
-}
+$('#new-configuration-submit').click(function () {
+  var c = {
+    name: $('#channel-name').val().toLowerCase(),
+    bot: $('#channel-bot').val().toLowerCase(),
+    oauth: $('#channel-oauth').val(),
+    autoconnect: document.querySelector('#channel-autoconnect').checked,
+    silent: document.querySelector('#channel-notifications').checked,
+    standupReminder: document.querySelector('#channel-notification-break').checked
+  }
 
-if (document.querySelector('#new-configuration-cancel')) {
-  document.querySelector('#new-configuration-cancel').addEventListener('click', configurationCancel)
-}
+  if (!c.name) {
+    $('#channel-name').focus()
+    return
+  }
+  if (!c.bot) {
+    $('#channel-bot').focus()
+  }
+  if (!c.oauth) {
+    $('#channel-oauth').focus()
+  }
+
+  if (!c.name.startsWith('#')) {
+    c.name = `'#${c.name}`
+  }
+  ipcRenderer.send('save-configuration', c)
+})
+
+$('#new-configuration-cancel').click(function () {
+  console.log('configurationCancel()')
+  remote.getCurrentWindow().loadFile('src/pages/index.html')
+})
 
 $('#export-configuration-button').click(function () {
   ipcRenderer.send('export-command', '')
