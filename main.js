62629069f9b9679a46eeb93c47a60261769075b2
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-08 21:03:32 +0200

    Whisper to the user not the channel

--- main.js
+++ main.js
@@ -167,15 +167,15 @@ function onMessageHandler (target, context, msg, self) {
 
 function onJoinHandler (channel, username, self) {
   console.log(`onJoinHandler(${channel}, ${username}, ${self})`)
-  // if (self || username === global.config.name.replace('#', '')) { return }
-  // if (!global.config.greetedUsers) { global.config.greetedUsers = [] }
-  // let didGreetUser = global.config.greetedUsers.find(function (u) {
-  //   if (u === username) { return u }
-  // })
-  // if (didGreetUser) { return }
-  // global.config.greetedUsers.push(username)
-  // let msg = `Welcome @${username}, see ${global.config.prefix}commands for chat commands ;-)`
-  // chatClient.whisper(channel, msg)
+  if (self || username === global.config.name.replace('#', '')) { return }
+  if (!global.config.greetedUsers) { global.config.greetedUsers = [] }
+  let didGreetUser = global.config.greetedUsers.find(function (u) {
+    if (u === username) { return u }
+  })
+  if (didGreetUser) { return }
+  global.config.greetedUsers.push(username)
+  let msg = `Welcome @${username}, see ${global.config.prefix}commands for chat commands ;-)`
+  chatClient.whisper(username, msg)
 }
 
 function onHostedHandler (channel, username, viewers, autohost) {
