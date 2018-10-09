6e47c7074b1619509a4fa339af878a40441caa7a
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-09 19:58:06 +0200

    For testing purposes show the dev tools

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function createWindow () {
   mainWindow.loadFile('src/pages/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
