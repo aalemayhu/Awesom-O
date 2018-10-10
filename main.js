b9a8ce178292fba58445a74f7c4537b23b57eb7b
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-10 17:36:53 +0200

    Disable the dev tools for now

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function createWindow () {
   mainWindow.loadFile('src/pages/index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
